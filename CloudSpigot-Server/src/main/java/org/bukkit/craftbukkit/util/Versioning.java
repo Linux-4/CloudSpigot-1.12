740ec8b6f1d4db8c91b6a1f37723d4196c6872ca
Server24-7 <tim@server24-7.eu>
2019-06-07 16:36:59 +0200

    fix versioning

--- CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
+++ CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
@@ -11,7 +11,7 @@ public final class Versioning {
     public static String getBukkitVersion() {
         String result = "Unknown-Version";
 
-        InputStream stream = Bukkit.class.getClassLoader().getResourceAsStream("META-INF/maven/com.destroystokyo.paper/paper-api/pom.properties");
+        InputStream stream = Bukkit.class.getClassLoader().getResourceAsStream("META-INF/maven/eu.minewars.cloudspigot/cloudspigot-api/pom.properties");
         Properties properties = new Properties();
 
         if (stream != null) {
