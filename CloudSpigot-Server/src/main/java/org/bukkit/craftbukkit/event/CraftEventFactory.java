859e77ac153deb5f65193f05d1dbb7bd3480acdc
Server24-7 <tim@server24-7.eu>
2019-06-10 09:46:36 +0200

    fix

--- CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
+++ CloudSpigot-Server/src/main/java/org/bukkit/craftbukkit/event/CraftEventFactory.java
@@ -747,7 +747,8 @@ public class CraftEventFactory {
 			event = new EntityDamageByEntityEvent(damager.getBukkitEntity(), damagee.getBukkitEntity(), cause,
 					modifiers, modifierFunctions);
 			// CloudSpigot start - AntiRange
-			if (damager.getBukkitEntity() instanceof Player) {
+			if (damager.getBukkitEntity() instanceof Player
+					&& damagee.getBukkitEntity() instanceof org.bukkit.entity.LivingEntity) {
 				final Player player = (Player) damager.getBukkitEntity();
 				final org.bukkit.entity.LivingEntity entity = (org.bukkit.entity.LivingEntity) damagee
 						.getBukkitEntity();
